972c91eb0c5f30b0528a7b803408f93295357799
Steve Persch <steve.persch@pantheon.io>
2018-09-17 22:12:48 -0500

    https forcing

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -36,3 +36,14 @@ if (file_exists($local_settings)) {
  * modifying settings.php.
  */
 $settings['install_profile'] = 'openy';
+
+if (isset($_ENV['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+    $primary_domain = $_SERVER['HTTP_HOST'];
+    if ($_SERVER['HTTP_HOST'] != $primary_domain
+        || !isset($_SERVER['HTTP_USER_AGENT_HTTPS'])
+        || $_SERVER['HTTP_USER_AGENT_HTTPS'] != 'ON' ) {
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+        exit();
+    }
+}
